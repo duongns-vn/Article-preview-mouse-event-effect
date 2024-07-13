e5eba1cce9f6ce40d6e47c313641cb8b2cf44ed0
duongns-vn <duongns.fx@gmail.com>
2024-07-13 13:51:07 +0700

    Push

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const icon = document.querySelector('.share_icon');
+    const popUp = document.querySelector('.share_box');
+
+    let onShow = () => {
+        popUp.style.display = 'flex';
+        popUp.classList.add('fade-in-up');
+        popUp.classList.remove('fade-out-down');
+    };
+
+    let onHide = () => {
+        popUp.classList.add('fade-out-down');
+        popUp.classList.remove('fade-in-up');
+        setTimeout(() => {popUp.style.display = 'none';}, 300);
+    };
+
+    icon.onmouseover = onShow;
+    
+    icon.onmouseleave = () => {
+        setTimeout(() => !popUp.matches(':hover') && onHide(), 300);
+    };
+
+    popUp.onmouseleave = onHide;
+});
